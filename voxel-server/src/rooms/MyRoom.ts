991e6efcc52514619fb7d56ee26bd2336f760a47
Asher Levinstig <your-github-email@example.com>
2026-01-28 17:50:11 -0500

    Register selectClass message handler and stabilize room messaging

--- voxel-server/src/rooms/MyRoom.ts
+++ voxel-server/src/rooms/MyRoom.ts
@@ -22,7 +22,7 @@ export class MyRoom extends Room<MyRoomState> {
   }
 
   onDispose() {
-    console.log("roosssssssssssm", this.roomId, "disposing...");
+    console.log("roossssssssssssm", this.roomId, "disposing...");
   }
 
 }
